cbd7fd4dd5d167600f3ef1380d67e789187e79ce
Lee Dong Wook <sh95119@gmail.com>
2021-08-15 04:34:00 +0900

    FEAT: Product 엔티티에 생성시간, 수정시간 및 활성화 여부 속성을 추가하였다

--- src/main/java/com/carbonzero/App.java
+++ src/main/java/com/carbonzero/App.java
@@ -3,10 +3,12 @@ package com.carbonzero;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import com.github.dozermapper.core.DozerBeanMapperBuilder;
 import com.github.dozermapper.core.Mapper;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
