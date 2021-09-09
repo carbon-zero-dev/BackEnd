7bc1990402c3eadaa1d81abf92eaea6a4edf3124
EP <kjuioqq8@gmail.com>
2021-09-09 23:21:25 +0900

    CHORE: categoy id 제거

--- src/main/java/com/carbonzero/domain/Product.java
+++ src/main/java/com/carbonzero/domain/Product.java
@@ -51,8 +51,6 @@ public class Product extends BaseTime {
 
     private Integer carbonEmissions;
 
-    private Long categoryId;
-
     public void changeWith(Product source) {
         this.name = source.name;
         this.brand = source.brand;
